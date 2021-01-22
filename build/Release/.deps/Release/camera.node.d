de183804a51431a8ae27d3a50cd6a0ec2ec8b7b4
Jasper Vander Velden <jasper.vv@gmail.com>
2021-01-21 22:10:15 -0500

    Remove build folder

--- build/Release/.deps/Release/camera.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/camera.node := rm -rf "Release/camera.node" && cp -af "Release/obj.target/camera.node" "Release/camera.node"
--- build/Release/.deps/Release/obj.target/camera.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/camera.node := g++ -shared -pthread -rdynamic -m64  -Wl,-soname=camera.node -o Release/obj.target/camera.node -Wl,--start-group Release/obj.target/camera/node-addon.o -Wl,--end-group 
